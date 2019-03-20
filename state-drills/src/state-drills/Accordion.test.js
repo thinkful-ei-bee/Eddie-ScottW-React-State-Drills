83ad1c25de72434c6659a809580d53d293aed4b5
Scott Williams <scottcolemanwilliams@gmail.com>
2019-03-20 16:16:31 -0500

    added final tests and finished exercise

--- state-drills/src/state-drills/Accordion.test.js
+++ state-drills/src/state-drills/Accordion.test.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './Accordion';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
-const sections = [
-  {
-    title: 'Section 1',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-  },
-  {
-    title: 'Section 2',
-    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
-  },
-  {
-    title: 'Section 3',
-    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
-  },
-]
 
 describe(`Accordion Component`, () => {
+
+  const sections = [
+    {
+      title: 'Section 1',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+      title: 'Section 2',
+      content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+    },
+    {
+      title: 'Section 3',
+      content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+    },
+  ]
+
   it('renders without errors', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Accordion />, div);
@@ -26,13 +29,26 @@ describe(`Accordion Component`, () => {
   })
 
   it('renders an empty li when sections prop is not given', ()=>{
-    const tree = renderer.create(<Accordion />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const wrapper = shallow(<Accordion />);
+    expect(toJson(wrapper)).toMatchSnapshot();
   })
 
   it('renders no sections as active by default',() => {
-    const tree = renderer.create(<Accordion sections={sections}/>).toJSON();
-    expect(tree).toMatchSnapshot();
+    const wrapper = shallow(<Accordion sections={sections}/>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  })
+
+  it('opens a clicked section', () => {
+    const wrapper = shallow(<Accordion sections={sections} />);
+    wrapper.find('button').at(1).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
+  })
+
+  it('only opens the last section when multiple sections have been clicked', () => {
+    const wrapper = shallow(<Accordion sections={sections} />);
+    wrapper.find('button').at(1).simulate('click');
+    wrapper.find('button').at(2).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
   })
 
 })
