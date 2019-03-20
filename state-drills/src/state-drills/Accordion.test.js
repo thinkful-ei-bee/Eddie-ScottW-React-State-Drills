98a07d30611a0de893db90665cf5928387e4c6b2
Eddie Bueno <eddiebueno93@gmail.com>
2019-03-20 13:32:06 -0700

    added first 2 test for accordion

--- state-drills/src/state-drills/Accordion.test.js
+++ state-drills/src/state-drills/Accordion.test.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './Accordion';
+import renderer from 'react-test-renderer';
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
 describe(`Accordion Component`, () => {
   it('renders without errors', () => {
@@ -8,4 +24,15 @@ describe(`Accordion Component`, () => {
     ReactDOM.render(<Accordion />, div);
     ReactDOM.unmountComponentAtNode(div);
   })
+
+  it('renders an empty li when sections prop is not given', ()=>{
+    const tree = renderer.create(<Accordion />).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+
+  it('renders no sections as active by default',() => {
+    const tree = renderer.create(<Accordion sections={sections}/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+
 })
